ef6cf098817416b9f2285938e44f5c3b451d8e15
Circleiwan <80038106+Circleiwan@users.noreply.github.com>
2024-01-14 16:34:22 +0700

    Add files via upload

--- area_picker.py
+++ area_picker.py
@@ -1,50 +1,69 @@
 import numpy as np
 import cv2
-
+import time
 
 def nothing(x):
 	pass
 
-cap = cv2.VideoCapture('DirivingVideo.mp4')
+def trackbarSetup(width, height):
+	cv2.namedWindow("point_picker")
+	cv2.createTrackbar("TLx", "point_picker", 0, width, nothing)
+	cv2.createTrackbar("TLy", "point_picker", 0, height, nothing)
+	cv2.createTrackbar("TRx", "point_picker", 0, width, nothing)
+	cv2.createTrackbar("TRy", "point_picker", 0, height, nothing)
+	cv2.createTrackbar("BLx", "point_picker", 0, width, nothing)
+	cv2.createTrackbar("BLy", "point_picker", 0, height, nothing)
+	cv2.createTrackbar("BRx", "point_picker", 0, width, nothing)
+	cv2.createTrackbar("BRy", "point_picker", 0, height, nothing)
 
-width  = int(cap.get(3))
-height = int(cap.get(4))
+def warpArea(param):
+	pass
 
-cv2.namedWindow("point_picker")
-cv2.createTrackbar("TLx", "point_picker", 0, width, nothing)
-cv2.createTrackbar("TLy", "point_picker", 0, height, nothing)
-cv2.createTrackbar("TRx", "point_picker", 0, width, nothing)
-cv2.createTrackbar("TRy", "point_picker", 0, height, nothing)
-cv2.createTrackbar("BLx", "point_picker", 0, width, nothing)
-cv2.createTrackbar("BLy", "point_picker", 0, height, nothing)
-cv2.createTrackbar("BRx", "point_picker", 0, width, nothing)
-cv2.createTrackbar("BRy", "point_picker", 0, height, nothing)
+def ROIpick(vidSource):
+	cap = cv2.VideoCapture(vidSource)
+	width  = int(cap.get(3))
+	height = int(cap.get(4))
 
-while(cap.isOpened()):
+	trackbarSetup(width, height)
 
-	ret, frame = cap.read()
-	if not ret:
-		cap = cv2.VideoCapture("DirivingVideo.mp4")
-		continue
-		
-	TLx = cv2.getTrackbarPos('TLx', 'point_picker')
-	TLy = cv2.getTrackbarPos('TLy', 'point_picker')
-	TRx = cv2.getTrackbarPos('TRx', 'point_picker')
-	TRy = cv2.getTrackbarPos('TRy', 'point_picker')
-	BLx = cv2.getTrackbarPos('BLx', 'point_picker')
-	BLy = cv2.getTrackbarPos('BLy', 'point_picker')
-	BRx = cv2.getTrackbarPos('BRx', 'point_picker')
-	BRy = cv2.getTrackbarPos('BRy', 'point_picker')
+	while(cap.isOpened()):
+		ret, frame = cap.read()
 
-	cv2.line(frame, (TLx, TLy), (TRx, TRy), (0,255,0), 2)
-	cv2.line(frame, (TRx, TRy), (BRx, BRy), (0,255,0), 2)
-	cv2.line(frame, (BRx, BRy), (BLx, BLy), (0,255,0), 2)
-	cv2.line(frame, (BLx, BLy), (TLx, TLy), (0,255,0), 2)
+		if not ret:
+			cap = cv2.VideoCapture(vidSource)
+			continue
+			
+		TLx = cv2.getTrackbarPos('TLx', 'point_picker')
+		TLy = cv2.getTrackbarPos('TLy', 'point_picker')
+		TRx = cv2.getTrackbarPos('TRx', 'point_picker')
+		TRy = cv2.getTrackbarPos('TRy', 'point_picker')
+		BLx = cv2.getTrackbarPos('BLx', 'point_picker')
+		BLy = cv2.getTrackbarPos('BLy', 'point_picker')
+		BRx = cv2.getTrackbarPos('BRx', 'point_picker')
+		BRy = cv2.getTrackbarPos('BRy', 'point_picker')
 
-	cv2.imshow('Original',frame)
+		cv2.line(frame, (TLx, TLy), (TRx, TRy), (0,255,0), 2)
+		cv2.line(frame, (TRx, TRy), (BRx, BRy), (0,255,0), 2)
+		cv2.line(frame, (BRx, BRy), (BLx, BLy), (0,255,0), 2)
+		cv2.line(frame, (BLx, BLy), (TLx, TLy), (0,255,0), 2)
 
-	if cv2.waitKey(10) & 0xFF == ord('q'):
-		break
+		cv2.imshow('Original',frame)
 
-cap.release()
-cv2.destroyAllWindows()
+		if cv2.waitKey(10) & 0xFF == ord('q'):
+			return(TLx, TLy, TRx, TRy, BLx, BLy, BRx, BRy)
+			break
+
+	cap.release()
+	cv2.destroyAllWindows()
+
+def saveParam(param):
+	filename = "savedParam" + str(time.time()) + ".txt"
+	f = open(filename, "w")
+	for i in param:
+		f.write(str(i))
+		f.write("\n")
+	f.close()
+
+feedback = ROIpick("DirivingVideo.mp4")
+print(feedback)
+saveParam(feedback)
